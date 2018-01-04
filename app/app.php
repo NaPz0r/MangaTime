7fca1a54aa35e7416b4528c17c7a2e2980a5f3ed
NaPz0r <ncozette@gmail.com>
2018-01-04 11:53:36 +0100

    v1.0.2

--- app/app.php
+++ app/app.php
@@ -14,3 +14,13 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
   'twig.path' => __DIR__ . '/../templates',
   'twig.options' => array('debug' => true)
 ));
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+	'db.options' => array(
+		'driver' => 'pdo_mysql',
+		'host' => 'localhost',
+		'dbname' => 'mangatime',
+		'user' => 'root',
+		'password' => ''
+	)
+));
---
+++ src/domain/classManga.php
@@ -0,0 +1,137 @@
+<?php
+
+class Mangas{
+
+    private $idManga;
+    private $nameManga;
+    private $datePublicationManga;
+    private $descriptionManga;
+    private $statusManga;
+    private $author;
+
+    public function __construct($nameManga){
+        $this->setNameManga($nameManga);        
+    }
+
+    /**
+    * Get the value of idManga
+    */ 
+    public function getIdManga()
+    {
+        return $this->idManga;
+    }
+
+    /**
+     * Set the value of idManga
+     *
+     * @return  self
+     */ 
+    public function setIdManga($idManga)
+    {
+        $this->idManga = $idManga;
+
+        return $this;
+    }
+
+    /**
+     * Set the value of nameManga
+     *
+     * @return  self
+     */ 
+    public function setNameManga($nameManga)
+    {
+            $this->nameManga = $nameManga;
+
+            return $this;
+    }
+
+    /**
+     * Get the value of nameManga
+     */ 
+    public function getNameManga()
+    {
+            return $this->nameManga;
+    }
+
+    /**
+     * Get the value of datePublicationManga
+     */ 
+    public function getDatePublicationManga()
+    {
+        return $this->datePublicationManga;
+    }
+
+    /**
+     * Set the value of datePublicationManga
+     *
+     * @return  self
+     */ 
+    public function setDatePublicationManga($datePublicationManga)
+    {
+        $this->datePublicationManga = $datePublicationManga;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of descriptionManga
+     */ 
+    public function getDescriptionManga()
+    {
+        return $this->descriptionManga;
+    }
+
+    /**
+     * Set the value of descriptionManga
+     *
+     * @return  self
+     */ 
+    public function setDescriptionManga($descriptionManga)
+    {
+        $this->descriptionManga = $descriptionManga;
+
+        return $this;
+    }
+
+ 
+
+    /**
+     * Get the value of statusManga
+     */ 
+    public function getStatusManga()
+    {
+        return $this->statusManga;
+    }
+
+    /**
+     * Set the value of statusManga
+     *
+     * @return  self
+     */ 
+    public function setStatusManga($statusManga)
+    {
+        $this->statusManga = $statusManga;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of author
+     */ 
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set the value of author
+     *
+     * @return  self
+     */ 
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+}
